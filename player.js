9fffd39a117f17d68150e84ee239941d966b983a
Ramya <ramya.s2023cse@sece.ac.in>
2025-07-04 09:58:55 +0530

    second commit

--- player.js
+++ player.js
@@ -1,7 +1,9 @@
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+
+let score0 = 0;        
+let score1 = 0;        
+let currentScore = 0;  
+let activePlayer = 0;  
+let playing = true;    
 
 const diceEl = document.getElementById("dice");
 const score0El = document.getElementById("score--0");
@@ -21,11 +23,11 @@ function switchPlayer() {
   activePlayer = activePlayer === 0 ? 1 : 0;
   player0El.classList.toggle("player--active");
   player1El.classList.toggle("player--active");
-} 
+}
 
 function init() {
-  scores[0] = 0;
-  scores[1] = 0;
+  score0 = 0;
+  score1 = 0;
   currentScore = 0;
   activePlayer = 0;
   playing = true;
@@ -44,36 +46,52 @@ function init() {
   player1El.style.background = "";
 }
 
+
 btnRoll.addEventListener("click", function () {
   if (!playing) return;
-
   const dice = Math.trunc(Math.random() * 6) + 1;
   diceEl.src = `dice${dice}.jpg`;
-
-  if (dice !== 1) {
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-  } else {
-    switchPlayer();
+  switch (dice) {
+    case 1:
+      switchPlayer();
+      break;
+    default:
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+      break;
   }
 });
 
 btnHold.addEventListener("click", function () {
   if (!playing) return;
-
-  scores[activePlayer] += currentScore;
-  document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
-
-  if (scores[activePlayer] >= 100) {
-    playing = false;
-    document.querySelector(`.player--${activePlayer + 1}`).style.background = "#28a745";
-    diceEl.style.display = "none";
-  } else {
-    switchPlayer();
+  switch (activePlayer) {
+    case 0:
+      score0 += currentScore;
+      score0El.textContent = score0;
+      if (score0 >= 100) {
+        endGame(0);
+      } else {
+        switchPlayer();
+      }
+      break;
+    case 1:
+      score1 += currentScore;
+      score1El.textContent = score1;
+      if (score1 >= 100) {
+        endGame(1);
+      } else {
+        switchPlayer();
+      }
+      break;
   }
 });
 
+function endGame(winner) {
+  playing = false;
+  document.querySelector(`.player--${winner + 1}`).style.background = "#28a745";
+  diceEl.style.display = "none";
+}
+
 btnNew.addEventListener("click", init);
 
-// Start the game initially
 init();
